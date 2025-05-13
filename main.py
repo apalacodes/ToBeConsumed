8f4dac34b5e0e831df0996dbddf9a563457fea6b
apalacodes <apala004.t@gmail.com>
2025-05-14 01:17:52 +0545

    FrontendUpdates1

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Request
 from routers import books, movies
 
-app = FastAPI()
-app.include_router(books.router, prefix="/books", tags=["books"])
-app.include_router(movies.router, prefix="/movies", tags=["movies"])
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to ToBeConsumed!"}
+app = FastAPI(
+    title="ToBeConsumed",
+    description="A simple app to track books, movies, and shows you want to consume.",
+    version="1.0"
+)
+# -------------------new-------------------
+# Mount static directory
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Setup Jinja2 templates
+templates = Jinja2Templates(directory="templates")
+
+# --------------------------------------------
+
+app.include_router(books.router, prefix="/books", tags=["books"])
+# app.include_router(movies.router, prefix="/movies")
+
+# @app.get("/api/welcome")
+# def read_welcome():
+#     return {"message": "Welcome to ToBeConsumed!"}
+
+# Landing page route
+@app.get("/", response_class=HTMLResponse)
+async def serve_home(request: Request):
+    return templates.TemplateResponse("landing.html", {"request": request})
+
+@app.get("/index", response_class=HTMLResponse)
+async def landing(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
